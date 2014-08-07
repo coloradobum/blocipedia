0d261b38d419354ef6302a947be3b8b2d4bc8d49
Jeff Thomas <subskriptions@gmail.com>
2014-08-07 16:40:56 -0600

    Failing specs.

---
+++ spec/features/create_private_wikis_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+feature 'add public wikis', %q{
+  As a user
+  I want to be able to upgrade my account from a free plan to a paid plan
+  So I can create private wikis 
+} do
+  
+  scenario 'ability to upgrade free to premium account' do
+    user = FactoryGirl.create(:user)
+    user.confirmed_at = Time.now
+    user.save
+
+    visit root_path
+    click_on 'Sign in'
+    fill_in 'Email', with: user.email 
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    expect(page).to have_content("Upgrade to premium account")
+end
+
+  scenario 'create private wiki' do
+    pending
+    user = FactoryGirl.create(:user)
+    user.confirmed_at = Time.now
+    user.save
+
+    visit root_path
+    click_on 'Sign in'
+    fill_in 'Email', with: user.email 
+    fill_in 'Password', with: user.password
+    click_button 'Sign in'
+
+    click_on 'New wiki'
+    fill_in 'Title', with: "My Shiny Private Wiki"
+    fill_in 'Body', with: "the best wiki ever"
+    check 'Make wiki private'
+    click_on 'Save'
+
+    click_on 'Sign out'
+
+    visit wikis_path
+
+    expect(page).to not_have_content("My Shiny Private Wiki")
+  end
+end
